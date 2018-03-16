a26f11b2e02e3e842dd736d8c030d8b6c29a0ad1
thakersamveg608 <thakersamveg608@gmail.com>
2018-03-17 01:10:20 +0530

    First commit

---
+++ app/src/main/java/com/hackstrangely/hackstrangely/hackstrangely/hackstrangely/hackstrangely/hackstrangely/helpinghand/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.hackstrangely.hackstrangely.hackstrangely.hackstrangely.hackstrangely.hackstrangely.helpinghand;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
