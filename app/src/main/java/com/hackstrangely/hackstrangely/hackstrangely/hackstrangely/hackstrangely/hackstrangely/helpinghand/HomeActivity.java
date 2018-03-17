4c10e0299a69b90f686d7aa23b85e02e2efe747e
thakersamveg608 <thakersamveg608@gmail.com>
2018-03-18 02:05:37 +0530

    IBM Push Notification Integrated

--- app/src/main/java/com/hackstrangely/hackstrangely/hackstrangely/hackstrangely/hackstrangely/hackstrangely/helpinghand/HomeActivity.java
+++ app/src/main/java/com/hackstrangely/hackstrangely/hackstrangely/hackstrangely/hackstrangely/hackstrangely/helpinghand/HomeActivity.java
@@ -2,12 +2,146 @@ package com.hackstrangely.hackstrangely.hackstrangely.hackstrangely.hackstrangel
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.ibm.mobilefirstplatform.clientsdk.android.core.api.BMSClient;
+import com.ibm.mobilefirstplatform.clientsdk.android.push.api.MFPPush;
+import com.ibm.mobilefirstplatform.clientsdk.android.push.api.MFPPushException;
+import com.ibm.mobilefirstplatform.clientsdk.android.push.api.MFPPushNotificationListener;
+import com.ibm.mobilefirstplatform.clientsdk.android.push.api.MFPPushResponseListener;
+import com.ibm.mobilefirstplatform.clientsdk.android.push.api.MFPSimplePushNotification;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class HomeActivity extends AppCompatActivity {
+
+
+    private static final String TAG = HomeActivity.class.getSimpleName();
+
+    private MFPPushNotificationListener notificationListener;
+    private MFPPush push;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        // Initialize the SDK
+        BMSClient.getInstance().initialize(this, BMSClient.REGION_UK);
+//Initialize client Push SDK
+
+        push = MFPPush.getInstance();
+        push.initialize(getApplicationContext(), "8f270b6c-ee65-43dd-aec8-ba296400ecc2", "00cc07b8-130d-4da7-a8bf-eaef789f0ce0");
+
+        registerDevice();
+
+        notificationListener = new MFPPushNotificationListener() {
+            @Override
+            public void onReceive(final MFPSimplePushNotification message) {
+                Log.i(TAG, "Received a Push Notification: " + message.toString());
+            }
+        };
+
+
+    }
+
+
+    /**
+     * Called when the register device button is pressed.
+     * Attempts to register the device with your push service on Bluemix.
+     * If successful, the push client sdk begins listening to the notification listener.
+     * Also includes the example option of UserID association with the registration for very targeted Push notifications.
+     *
+     * @param view the button pressed
+     */
+    public void registerDevice() {
+
+        // Checks for null in case registration has failed previously
+        if(push==null){
+            push = MFPPush.getInstance();
+        }
+
+        Log.i(TAG, "Registering for notifications");
+
+        // Creates response listener to handle the response when a device is registered.
+        MFPPushResponseListener registrationResponselistener = new MFPPushResponseListener<String>() {
+            @Override
+            public void onSuccess(String response) {
+                // Split response and convert to JSON object to display User ID confirmation from the backend
+                String[] resp = response.split("Text: ");
+                try {
+                    JSONObject responseJSON = new JSONObject(resp[1]);
+                    setStatus("Device Registered Successfully with USER ID " + responseJSON.getString("userId"), true);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                Log.i(TAG, "Successfully registered for push notifications, " + response);
+                // Start listening to notification listener now that registration has succeeded
+                push.listen(notificationListener);
+            }
+
+            @Override
+            public void onFailure(MFPPushException exception) {
+                String errLog = "Error registering for push notifications: ";
+                String errMessage = exception.getErrorMessage();
+                int statusCode = exception.getStatusCode();
+
+                // Set error log based on response code and error message
+                if(statusCode == 401){
+                    errLog += "Cannot authenticate successfully with Bluemix Push instance, ensure your CLIENT SECRET was set correctly.";
+                } else if(statusCode == 404 && errMessage.contains("Push GCM Configuration")){
+                    errLog += "Push GCM Configuration does not exist, ensure you have configured GCM Push credentials on your Bluemix Push dashboard correctly.";
+                } else if(statusCode == 404 && errMessage.contains("PushApplication")){
+                    errLog += "Cannot find Bluemix Push instance, ensure your APPLICATION ID was set correctly and your phone can successfully connect to the internet.";
+                } else if(statusCode >= 500){
+                    errLog += "Bluemix and/or your Push instance seem to be having problems, please try again later.";
+                }
+
+                setStatus(errLog, false);
+                Log.e(TAG,errLog);
+                // make push null since registration failed
+                push = null;
+            }
+        };
+
+        // Attempt to register device using response listener created above
+        // Include unique sample user Id instead of Sample UserId in order to send targeted push notifications to specific users
+        push.registerDeviceWithUserId("Sample UserID",registrationResponselistener);
+    }
+
+    // If the device has been registered previously, hold push notifications when the app is paused
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (push != null) {
+            push.hold();
+        }
+    }
+
+    // If the device has been registered previously, ensure the client sdk is still using the notification listener from onCreate when app is resumed
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (push != null) {
+            push.listen(notificationListener);
+        }
+    }
+
+    /**
+     * Manipulates text fields in the UI based on initialization and registration events
+     * @param messageText String main text view
+     * @param wasSuccessful Boolean dictates top 2 text view texts
+     */
+    private void setStatus(final String messageText, boolean wasSuccessful){
+//        final TextView responseText = (TextView) findViewById(R.id.response_text);
+//        final TextView topText = (TextView) findViewById(R.id.top_text);
+//        final TextView bottomText = (TextView) findViewById(R.id.bottom_text);
+//        final TextView buttonText = (TextView) findViewById(R.id.button_text);
+//        final String topStatus = wasSuccessful ? "Yay!" : "Bummer";
+//        final String bottomStatus = wasSuccessful ? "You Are Connected" : "Something Went Wrong";
+
     }
 }
